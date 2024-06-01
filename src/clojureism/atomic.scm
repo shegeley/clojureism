299e1b0d3fb5079a90223b956af60c897dc00ce7
Grigory Shepelev <shegeley@gmail.com>
2024-06-01 18:27:10 +0300

    (clojureism atomic): swap! is now stolen from guile-ares-rs (@abcdw)

--- src/clojureism/atomic.scm
+++ src/clojureism/atomic.scm
@@ -6,5 +6,11 @@
 
 (define (reset! a val) (atomic-box-set! a val) val)
 
-(define (swap! a f . args)
- (let [(val (apply f (ref a) args))] (reset! a val) val))
+(define (swap! box proc)
+ "Stolen from ares. Thanks to @abcdw https://git.sr.ht/~abcdw/guile-ares-rs/tree/9194aeb/src/guile/ares/atomic.scm#L24"
+ (let loop ((old-value (atomic-box-ref box)))
+  (let* ((new-value (proc old-value))
+         (cas-value (atomic-box-compare-and-swap! box old-value new-value)))
+   (if (eq? old-value cas-value)
+    new-value
+    (loop cas-value)))))
